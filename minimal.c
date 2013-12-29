0cc60b1819725d3ece8f7393ececff343695197b
baptiste <baptiste@baptistes-MacBook-Pro-2.local>
2013-12-29 18:36:35 -0300

    replaced malloc

--- minimal.c
+++ minimal.c
@@ -42,16 +42,13 @@ int fwrap(unsigned ndim, const double *x, void *fdata, unsigned fdim, double *fv
 int main(int argc, char** argv){
 
   const int fdim = 2;
-  std::vector<double> memory_block(fdim);
-  double* data_pointer = &memory_block[0];
-
-  // initialise an Armadillo matrix to use external memory
-  Mat<double>  result(data_pointer, 1, fdim, false);
+  // initialise the vectors to store integration results
+  std::vector<double> integral(fdim);
+  std::vector<double> error(fdim);
+  double* integral_pt = &integral[0];
+  double* error_pt = &error[0];
   
   double xmin[3] = {-2,-2,-2}, xmax[3] = {2,2,2}, sigma = 0.5;
-  double *val, *err;
-
-   err =  (double *) malloc(sizeof(double) * 2);
 
    /* int hcubature(unsigned fdim, integrand f, void *fdata, */
    /*               unsigned dim, const double *xmin, const double *xmax,  */
@@ -59,10 +56,13 @@ int main(int argc, char** argv){
    /*               error_norm norm, */
    /*               double *val, double *err); */
 
-   hcubature(fdim, fwrap, &sigma, 3, xmin, xmax, 0, 0, 1e-4, ERROR_INDIVIDUAL, data_pointer, err);
+   hcubature(fdim, fwrap, &sigma, 3, xmin, xmax, 0, 0, 1e-4, ERROR_INDIVIDUAL, integral_pt, error_pt);
 
-   result.print("result:");
-   
+  // initialise an Armadillo matrix to use external memory
+  Mat<double>  result(integral_pt, 1, fdim, false);
+  // do things with the matrix...
+  result.print("result:");
+  
    return 0;
    
 }
